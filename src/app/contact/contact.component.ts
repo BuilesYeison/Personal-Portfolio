a0eb3ff8fc8f981cf42eb0f186f0334d8e8c5129
BuilesYeison <y.builes.uribe@gmail.com>
2021-05-18 09:53:24 -0500

    aos implemented on contact section

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -11,8 +11,24 @@ export class ContactComponent implements OnInit {
   textColor:string;
   backgroundColor:string;
   constructor(private darkmode:DarkmodeService) { }
-  openWindow(){
-    window.open("https://www.github.com")
+  openWindow(option:string){//open a new web page depending in the selection
+    switch(option){
+      case 'github':
+        window.open("https://github.com/BuilesYeison");
+        break;
+      case 'youtube':
+        window.open("https://www.youtube.com/channel/UC0-0QRFUH9g221kSvRs9plA");
+        break;
+      case 'twitter':
+        window.open("https://twitter.com/BuilesYeison");
+        break;
+      case 'linkedin':
+        window.open("https://www.linkedin.com/in/builesyeison/");
+        break;
+      default:
+        window.open("https://github.com/BuilesYeison");
+        break;
+    }
   }
   ngOnInit(): void {
   }
